d1c7891ede8234b05cce1a31a9a5ad9a76cdfcd5
woodser <woodser@protonmail.com>
2023-03-14 16:33:30 -0400

    support tor proxy uri

--- src/wallet/monero_wallet_full.h
+++ src/wallet/monero_wallet_full.h
@@ -185,6 +185,7 @@ namespace monero {
     bool is_view_only() const override { return m_w2->watch_only(); }
     void set_daemon_connection(const std::string& uri, const std::string& username = "", const std::string& password = "") override;
     void set_daemon_connection(const boost::optional<monero_rpc_connection>& connection) override;
+    void set_daemon_proxy(const std::string& uri = "") override;
     boost::optional<monero_rpc_connection> get_daemon_connection() const override;
     bool is_connected_to_daemon() const override;
     bool is_daemon_synced() const override;
