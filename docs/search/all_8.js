c9a5ddefc6feed88f133a29ff47cef10aefccf7d
woodser <woodser@protonmail.com>
2024-01-27 14:31:52 -0500

    update api docs

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['make_5fmultisig_0',['make_multisig',['../classmonero_1_1monero__wallet__full.html#ac28b11afbadc722da14b46302a061c03',1,'monero::monero_wallet_full::make_multisig()'],['../classmonero_1_1monero__wallet.html#a04d5bd95d2c595a103186cb116c6bbd3',1,'monero::monero_wallet::make_multisig()']]],
+  ['make_5fmultisig_0',['make_multisig',['../classmonero_1_1monero__wallet.html#a04d5bd95d2c595a103186cb116c6bbd3',1,'monero::monero_wallet::make_multisig()'],['../classmonero_1_1monero__wallet__full.html#ac28b11afbadc722da14b46302a061c03',1,'monero::monero_wallet_full::make_multisig()']]],
   ['merge_5ftx_1',['merge_tx',['../namespacemonero.html#aa7b02b602d32e83c095c58c153c1e8b1',1,'monero']]],
   ['monero_2',['monero',['../namespacemonero.html',1,'']]],
   ['monero_5faccount_3',['monero_account',['../structmonero_1_1monero__account.html',1,'monero']]],
