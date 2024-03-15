e72f9d0858098c5624ca17fa9f51d7445985bb70
Sidney <j.argenio@studenti.unipi.it>
2024-03-15 22:54:03 +0100

    Fix

--- test/monero_light_tests.cpp
+++ test/monero_light_tests.cpp
@@ -24,14 +24,15 @@ int main(int argc, const char* argv[]) {
   MINFO("===== Light Tests =====");
   MINFO("===== Create wallet from keys =====");
   monero_wallet_config wallet_config;
-  wallet_config.m_seed = "hefty value later extra artistic firm radar yodel talent future fungal nutshell because sanity awesome nail unjustly rage unafraid cedar delayed thumbs comb custom sanity";
+  //wallet_config.m_seed = "hefty value later extra artistic firm radar yodel talent future fungal nutshell because sanity awesome nail unjustly rage unafraid cedar delayed thumbs comb custom sanity";
+  wallet_config.m_seed = "silk mocked cucumber lettuce hope adrenalin aching lush roles fuel revamp baptism wrist long tender teardrop midst pastry pigment equip frying inbound pinched ravine frying";
   wallet_config.m_primary_address = "A1y9sbVt8nqhZAVm3me1U18rUVXcjeNKuBd1oE2cTs8biA9cozPMeyYLhe77nPv12JA3ejJN3qprmREriit2fi6tJDi99RR";
   wallet_config.m_private_view_key = "198820da9166ee114203eb38c29e00b0e8fc7df508aa632d56ead849093d3808";
   wallet_config.m_path = "MyLightWalletRestored";
   wallet_config.m_password = "supersecretpassword123";
   wallet_config.m_network_type = monero_network_type::TESTNET;
   wallet_config.m_server = monero_rpc_connection("http://localhost:8443", "superuser", "abctesting123");
-  wallet_config.m_restore_height = 380104;
+  wallet_config.m_restore_height = 2367336;
   wallet_config.m_seed_offset = "";
   monero_wallet_light* wallet_restored = monero_wallet_light::create_wallet(wallet_config);
   MINFO("===== Wallet Light created successfully =====");
