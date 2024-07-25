19c10b71589d06947e0cd747407c38ad12adf657
preland <prelandofficialmusic@gmail.com>
2024-07-24 21:51:07 -0400

    finished build

--- macos_universal/build.sh
+++ macos_universal/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd .. && ./bin/update_submodules.sh
-cd ../external/monero-project && make depends target=x86_64-apple-darwin11
-cd ../external/monero-project && make depends target=aarch64-apple-darwin11
-cd universal && ./combine_and_build_all.sh
+cd external/monero-project && make depends target=x86_64-apple-darwin11
+make depends target=aarch64-apple-darwin11
+
+cd ../../macos_universal/universal && ./combine_and_build_all.sh
--- macos_universal/universal/combine_and_build_all.sh
+++ macos_universal/universal/combine_and_build_all.sh
@@ -5,4 +5,4 @@ cd ../arm && ./clean.sh && ./build.sh
 # build x86
 cd ../x86 && ./clean.sh && ./build.sh
 # combine
-./combine.sh
+cd ../universal && ./combine.sh
