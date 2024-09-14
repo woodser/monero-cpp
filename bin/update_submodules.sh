cca26534a607e17f980cb3c4c569bb730abe1370
woodser <woodser@protonmail.com>
2024-09-14 10:40:12 -0400

    preserve branches on submodule update

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
 # initialize submodules recursively
-git submodule update --init --force --recursive
-
-# update monero-project
-cd ./external/monero-project
-git checkout master
-git pull --ff-only origin master
-cd ../../
+git submodule update --init --recursive
