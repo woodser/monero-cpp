988b870885779dc31542192f662543b4f2c6f349
nsec1 <167650977+nsec1@users.noreply.github.com>
2024-09-18 10:52:34 -0300

    optional static libraries building

--- bin/build_libmonero_cpp.sh
+++ bin/build_libmonero_cpp.sh
@@ -1,9 +1,43 @@
 #!/bin/sh
 
+export HOST_NCORES=${HOST_NCORES-$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)}
+BUILD_DIR="$(pwd)/build"
+INSTALL_DIR="$BUILD_DIR/install"
+export CMAKE_PREFIX_PATH=$INSTALL_DIR${CMAKE_PREFIX_PATH+:$CMAKE_PREFIX_PATH}
+export OPENSSL_ROOT_DIR=$INSTALL_DIR
+echo "HOST_NCORES=$HOST_NCORES in $0"
+echo "CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH in $0"
+echo "OPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR in $0"
+
+[ -d $INSTALL_DIR ] || mkdir -p $INSTALL_DIR
+
+(cd external/boost && \
+./bootstrap.sh --prefix=$INSTALL_DIR && ./b2 -j$HOST_NCORES \
+--with-chrono \
+--with-date_time \
+--with-filesystem \
+--with-program_options \
+--with-regex \
+--with-serialization \
+--with-system \
+--with-thread \
+--with-locale \
+link=static \
+cxxflags=-fPIC cflags=-fPIC \
+install) && \
+
+(cd external/openssl && \
+./Configure no-apps no-afalgeng no-docs no-ui-console no-shared --prefix=$INSTALL_DIR --libdir=lib && \
+make -j$HOST_NCORES && make install) && \
+
+(cd external/libsodium && \
+./configure --enable-shared=no --with-pic=yes --prefix=$INSTALL_DIR && \
+make -j$HOST_NCORES && make check && make install) && \
+
 # build monero-project dependencies
 cd ./external/monero-project/ || exit 1
 git submodule update --init --force || exit 1
-HOST_NCORES=$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
+
 if [[ "$OSTYPE" == "msys" ]]; then
     bit=$(getconf LONG_BIT)
     if [ "$bit" == "64" ]; then
@@ -16,13 +50,19 @@ elif [[ "$OSTYPE" == "cygwin" ]]; then
     exit 1
 else
     # OS is not windows
-    make release-static -j$HOST_NCORES || exit 1
+    MONERO_BUILD_DIR="build/release"
+    test -d $MONERO_BUILD_DIR || mkdir -p $MONERO_BUILD_DIR
+    (cd $MONERO_BUILD_DIR && \
+     cmake -D STATIC=ON -D BUILD_64=ON -D CMAKE_BUILD_TYPE=Release \
+           -D OPENSSL_ROOT_DIR=$OPENSSL_ROOT_DIR \
+           -D USE_DEVICE_TREZOR=OFF \
+     ../.. && make -j$HOST_NCORES wallet) || exit 1
 fi
 cd ../../
 
 # build libmonero-cpp shared library
 mkdir -p build && 
 cd build && 
-cmake .. && 
+cmake $@ .. &&
 cmake --build . && 
-make .
+make -j$HOST_NCORES .
