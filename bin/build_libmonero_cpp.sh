21c3e52c9aaa95a8129369cc9bdd49eb0252fa9b
everoddandeven <everoddandeven@protonmail.com>
2026-06-13 17:14:42 +0200

    add ci build
    * Fix boost dependencies build
    * Bump minimum required boost version to v1.62
    * Improve build script

--- bin/build_libmonero_cpp.sh
+++ bin/build_libmonero_cpp.sh
@@ -6,14 +6,40 @@ git submodule update --init --force || exit 1
 HOST_NCORES=$(nproc 2>/dev/null || shell nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || echo 1)
 if [[ $(uname -s) == "MINGW64_NT"* || $(uname -s) == "MSYS"* ]]; then
     bit=$(getconf LONG_BIT)
+    FOLDER=$(cd ${MINGW_PREFIX}/.. && pwd -W)
     if [ "$bit" == "64" ]; then
-        make release-static-win64 -j$HOST_NCORES || exit 1
+        cmake -G "MSYS Makefiles" \
+            -D STATIC=ON \
+            -D ARCH="x86-64" \
+            -D BUILD_64=ON \
+            -D CMAKE_BUILD_TYPE=Release \
+            -D BUILD_TAG="win-x64" \
+            -D CMAKE_TOOLCHAIN_FILE=../../cmake/64-bit-toolchain.cmake \
+            -D MSYS2_FOLDER="$FOLDER" \
+            -D USE_DEVICE_TREZOR=OFF \
+            ../../
+            
+        make -j$HOST_NCORES wallet cryptonote_protocol || exit 1
     else
-        make release-static-win32 -j$HOST_NCORES || exit 1
+        cmake -G "MSYS Makefiles" \
+            -D STATIC=ON \
+            -D ARCH="i686" \
+            -D BUILD_64=OFF \
+            -D CMAKE_BUILD_TYPE=Release \
+            -D BUILD_TAG="win-x32" \
+            -D CMAKE_TOOLCHAIN_FILE=../../cmake/32-bit-toolchain.cmake \
+            -D MSYS2_FOLDER="$FOLDER" \
+            -D USE_DEVICE_TREZOR=OFF \
+            ../../
+            
+        make -j$HOST_NCORES wallet cryptonote_protocol || exit 1
     fi
 else
     # OS is not windows
-    make release-static -j$HOST_NCORES || exit 1
+    mkdir -p build/release &&
+    cd build/release &&
+    cmake -DCMAKE_BUILD_TYPE=Release ../../
+    make -j$HOST_NCORES wallet cryptonote_protocol || exit 1
 fi
 cd ../../
 
